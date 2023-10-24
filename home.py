423ed705cef428921fec89e429fe360a6ca10664
elsilc <148568584+elsilc@users.noreply.github.com>
2023-10-24 23:08:29 +0700

    home.py

--- home.py
+++ home.py
@@ -25,7 +25,7 @@ def main():
     if choice == "Home":
         col1, col2 = st.columns([0.4,0.6])
         with col1:
-            st.image('gambar/kapal.jpg')
+            st.image('kapal.jpg')
         with col2:
             st.write('RMS Titanic adalah sebuah kapal penumpang super Britania Raya yang tenggelam di Samudra Atlantik Utara pada tanggal 15 April 1912 setelah menabrak sebuah gunung es pada pelayaran perdananya dari Southampton, Inggris ke New York City. Tenggelamnya Titanic mengakibatkan kematian sebanyak 1514 orang dalam salah satu bencana maritim masa dama paling mematikan sepanjang sejarah. Titanic merupakan kapal terbesar di dunia pada pelayaran perdananya. Satu dari tiga kapal samudra kelas Olympic dioperasikan oleh White Star Line. Kapal ini dibangun pada 1909 sampai 1911 oleh galangan kapal Harland and Wolff di Belfast. Kapal ini sanggup mengangkut 2,224 penumpang.')
         
